d5991dcc6b5ad7ca7ea2c8d0734cf4f4204c7247
Zak Raicik <raicik.zach@Gmail.com>
2023-05-23 20:31:51 -0700

    Added blank screen for non web3 enabled browsers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import VotingComponent from "./components/VotingComponent";
+
 import {
   CssBaseline,
   ThemeProvider,
@@ -13,46 +14,62 @@ import darkTheme from "./themes/DarkTheme"; // Make sure to create a DarkTheme
 
 function App() {
   const [isDark, setIsDark] = useState(true);
+  const [isWeb3BrowserDetected, setIsWeb3BrowserDetected] = useState(false);
 
+  console.log(isWeb3BrowserDetected);
   const handleThemeChange = () => {
     setIsDark(!isDark);
   };
 
+  useEffect(() => {
+    if (typeof window.ethereum !== 'undefined') {
+      setIsWeb3BrowserDetected(true);
+    } else {
+      setIsWeb3BrowserDetected(false);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <CssBaseline />
-      <Box
-        sx={{
-          minHeight: "100vh",
-          position: "relative",
-        }}
-      >
-        <VotingComponent />
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            gap: "8px",
-            p: 2,
-          }}
-        >
+      {!isWeb3BrowserDetected ? (
+        <p>test</p>
+      ) : (
+        <Box>
+          <CssBaseline />
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              width: "100px", // Set a specific width here
+              minHeight: "100vh",
+              position: "relative",
             }}
           >
-            <Typography variant="caption">Dark Mode</Typography>
-            <Switch checked={isDark} onChange={handleThemeChange} />
+            <VotingComponent />
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                gap: "8px",
+                p: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  width: "100px", // Set a specific width here
+                }}
+              >
+                <Typography variant="caption">Dark Mode</Typography>
+                <Switch checked={isDark} onChange={handleThemeChange} />
+              </Box>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      )}
     </ThemeProvider>
   );
 }
